241724cfe535e5bb3d3e349785f142ab932fba82
Ennio-M <marrese.ennio@gmail.com>
2022-05-26 01:38:17 +0200

    bonus

--- script/script.js
+++ script/script.js
@@ -2,15 +2,38 @@ const app = new Vue({
     el: '#app',
     data: {
         albums: [],
+        genres: [],
+        authors: [],
+        selectedGenre: '',
+        selectedAuthor: '',
         apiPath: './server.php'
     },
     methods: {
-
+        search() {
+            this.getData(this.selectedGenre);
+        },
+        getData(genre = null) {
+            let path = this.apiPath;
+            if(genre) {
+                path = `${this.apiPath}?genre=${genre}`;
+            }
+            axios.get(path).then((res) => {
+                this.albums = res.data;
+                //console.log(this.albums);
+                if(this.genres.length < 1) {
+                    this.albums.forEach((album) => {
+                        if(!this.genres.includes(album.genre)) {
+                            this.genres.push(album.genre);
+                        }
+                        if(!this.authors.includes(album.author)) {
+                            this.authors.push(album.author);
+                        }
+                    })
+                }
+            }).catch((error) => {console.log(error)})
+        }
     },
     mounted() {
-        axios.get(this.apiPath).then((res) => {
-            this.albums = res.data;
-            //console.log(this.albums);
-        })
+        this.getData();
     }
 });
--- server.php
+++ server.php
@@ -1,7 +1,18 @@
 <?php
     include __DIR__.'/database.php';
     //var_dump($data);
+    $result = [];
+
+    if(isset($_GET['genre'])) {
+        foreach($data as $album) {
+            if($album['genre'] == $_GET['genre']) {
+                $result[] = $album;
+            }
+        }
+    } else {
+        $result = $data;
+    }
 
     header('Content-Type: application/json');
-    echo json_encode($data);
+    echo json_encode($result);
 ?>
